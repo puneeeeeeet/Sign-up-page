07aa784a02f7b39a2c9762af7597e22961763a26
puneeeeeeet <puneetchhabra1607@gmail.com>
2023-01-10 00:47:05 +0530

    changes the server port

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ app.post("/failure",function(req,res){
     res.redirect("/")
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("server working on port 3000");
 })
 
